373a260cfd39cc52d48ba254022d3362995a16f3
Anas SMAILI <anassmaili9@gmail.com>
2025-12-25 17:02:41 +0100

    fix data_in_index.c

--- algo/data_in_index.c
+++ algo/data_in_index.c
@@ -6,37 +6,30 @@
 /*   By: asmaili <asmaili@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/22 19:53:13 by asmaili           #+#    #+#             */
-/*   Updated: 2025/12/22 22:55:53 by asmaili          ###   ########.fr       */
+/*   Updated: 2025/12/25 17:02:29 by asmaili          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
-static void	fill_min(int *min, int i, t_stack *tmp)
+static void	fill_min(int *min, int i, int *data, int size)
 {
 	int	j;
-	int	min_found;
 
 	min[i] = i;
 	j = 0;
-	while (j < tmp->size - 1)
+	while (j < size)
 	{
-		if (j == tmp->size - 1 && tmp->size < tmp->capacity)
-			break ;
-		if (tmp->data[i] > tmp->data[j])
+		if (data[i] > data[j])
+		{
 			min[i] = j;
+			data[j] = -2147483648;
+		}
 		j += 1;
 	}
-	min_found = tmp->data[min[i]];
-	while (tmp->data[tmp->size - 1] != min_found)
-	{
-		if (j < (tmp->size - 1) / 2)
-			r_without_write(tmp);
-		else
-			rr_without_write(tmp);
-	}
-	tmp->size -= 1;
+	if (min[i] == i)
+		data[i] = -2147483648;
 }
 
 static void	fill_data_with_index(t_stack *a, int *min)
@@ -50,34 +43,36 @@ static void	fill_data_with_index(t_stack *a, int *min)
 		j = 0;
 		while (min[j] != i)
 			j += 1;
-		a->data[i++] = min[j];
+		a->data[i++] = j;
 	}
 }
 
 int	tranform_data_in_index(t_stack *a)
 {
-	t_stack	*tmp;
-	int		*min;
-	int		i;
+	int	*data;
+	int	*min;
+	int	size;
+	int	i;
 
+	size = a->capacity;
 	min = malloc(sizeof(int) * a->capacity);
-	tmp = malloc(sizeof(t_stack));
-	if (!min || !tmp)
-		return (free(min), free(tmp), 0);
+	data = malloc(sizeof(int) * a->capacity);
+	if (!min || !data)
+		return (free(min), free(data), 0);
 	i = 0;
 	while (i < a->capacity)
 	{
-		tmp->data[i] = a->data[i];
+		data[i] = data[i];
 		i += 1;
 	}
 	i = 0;
-	while (i < a->capacity)
+	while (i < size)
 	{
-		fill_min(min, i, tmp);
+		fill_min(min, i, data, size);
 		i += 1;
 	}
 	fill_data_with_index(a, min);
-	return (free(min), free(tmp), 1);
+	return (free(min), free(data), 1);
 }
 
 int	main(int ac, char **av)
@@ -102,14 +97,12 @@ int	main(int ac, char **av)
 	while (i < a->capacity)
 		printf("%d ", a->data[i++]);
 	printf("\n");
-	/*push_half_of_a_data_to_b(a, b);
+	/* push_half_of_a_data_to_b(a, b);
 	i = 0;
 	while (i < a->size)
 		printf("\nafter push half STACK a : %d\n", a->data[i++]);
 	i = 0;
 	while (i < b->size)
-		printf("after push half STACK b : %d\n", b->data[i++]);
-	return (0);*/
+		printf("after push half STACK b : %d\n", b->data[i++]); */
+	return (0);
 }
-
-
